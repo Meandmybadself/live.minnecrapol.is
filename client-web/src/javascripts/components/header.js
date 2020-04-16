3edcebfff15f3436a49ad60d264da855c8ba31b2
Max Thorson <max@thorsonmscott.com>
2020-04-15 23:11:15 -0500

    fix duplicate nav, add home link, basic mobile nav

--- client-web/src/javascripts/components/header.js
+++ client-web/src/javascripts/components/header.js
@@ -1,44 +1,70 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useLocation, useHistory, useRouteMatch } from 'react-router-dom'
 
 import { useAuth } from 'context/auth'
 import { useTheme } from 'context/theme'
 
 import logo from 'images/crappy-logo.png'
 
-export const Header = () => {
-  const { user, signOut } = useAuth()
-  const { dark, toggleDark } = useTheme()
-  const location = useLocation()
+const NavLink = ({ to, className, onNavigate = () => {}, children }) => {
+  const history = useHistory()
+  const match = useRouteMatch({ path: to, exact: true })
 
-  let howToStreamLink
-  if (location.pathname === '/how-to-stream') {
-    howToStreamLink = (
-      <a className="active">How to Stream</a>
-    )
-  } else {
-    howToStreamLink = (
-      <Link to="/how-to-stream">How to Stream</Link>
-    )
+  const onClick = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    if (!match) {
+      history.push(to)
+
+      onNavigate()
+    }
   }
 
   return (
+    <a
+      href={ to }
+      className={ `${className} ${match ? 'active' : ''}` }
+      onClick={ onClick }
+    >
+      { children }
+    </a>
+  )
+}
+
+export const Header = () => {
+  const [ mobileOpen, setMobileOpen ] = useState(false)
+  const { user, signOut } = useAuth()
+  const { dark, toggleDark } = useTheme()
+
+  return (
     <header>
-      <img className="logo" src={ logo } />
-      <h1>MINNECRAPOLIS LIVE!</h1>
+      <NavLink to="/" className="logo-link" onNavigate={ () => setMobileOpen(false) }>
+        <img className="logo" src={ logo } />
+        <h1>MINNECRAPOLIS LIVE!</h1>
+      </NavLink>
 
-      { !user &&
-        <Link to="/sign-in">Sign In</Link>
-      }
+      <nav className={ `${mobileOpen ? 'mobile-open' : ''}` }>
 
-      { user &&
-        <>
-          { howToStreamLink }
-          <a onClick={ signOut }>Sign Out</a>
-        </>
-      }
+        { !user && <NavLink to="/sign-in" onNavigate={ () => setMobileOpen(false) }>Sign In</NavLink> }
 
-      <a onClick={ toggleDark }>{ !dark ? 'Dark' : 'Light' }</a>
+        { user &&
+          <>
+            <NavLink to="/how-to-stream" onNavigate={ () => setMobileOpen(false) }>How to Stream</NavLink>
+            <a onClick={ signOut }>Sign Out</a>
+          </>
+        }
+
+        <a onClick={ toggleDark }>{ !dark ? 'Dark' : 'Light' }</a>
+
+      </nav>
+
+      <a className={ `mobile-toggle ${mobileOpen ? 'active' : ''}`} onClick={ () => setMobileOpen(!mobileOpen) }>
+        <div className="top-bun" />
+        <div className="patty" />
+        <div className="bottom-bun" />
+      </a>
+
     </header>
   )
 }
