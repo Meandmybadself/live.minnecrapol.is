99f4cf32e5c05a1e17fc93172b4eba923677fe2c
Max Thorson <max@thorsonmscott.com>
2020-07-08 11:54:25 -0500

    fixed that quick

--- client-web/src/javascripts/components/sign-in-with-slack.js
+++ client-web/src/javascripts/components/sign-in-with-slack.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SignInWithSlack = () => {
   const href =
-    `https://slack.com/oauth/authorize?scope=identity.basic,identity.avatar&client_id=3364554065.1020069085329&redirect_uri=${SLACK_REDIRECT}`
+    `https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.avatar&client_id=3364554065.1020069085329&redirect_uri=${SLACK_REDIRECT}`
   return (
     <a className="sign-in-with-slack" href={ href }>
       <img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" />
--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -4,22 +4,24 @@ const User = require('../schemas/user')
 const { SlackOAuthWebClient, SlackBotWebClient } = require('../utilities/slack')
 
 exports.signInWithSlack = async (code, redirect_uri) => {
-  const oAuthResponse = await SlackOAuthWebClient.oauth.access({
+  const oAuthResponse = await SlackOAuthWebClient.oauth.v2.access({
     client_id: process.env.MINNE_LIVE_SLACK_CLIENT_ID,
     client_secret: process.env.MINNE_LIVE_SLACK_CLIENT_SECRET,
     code,
     redirect_uri
   })
 
-  const userInfoResponse = await SlackBotWebClient.users.info({ user: oAuthResponse.user_id })
+  const userInfoResponse = await SlackBotWebClient.users.info({ user: oAuthResponse.authed_user.id })
+  
+  const { id: user_id, team_id, profile } = userInfoResponse.user
 
   const userData = {
-    user_id: oAuthResponse.user_id,
-    team_id: oAuthResponse.team_id,
-    name: oAuthResponse.user.name,
-    display_name: userInfoResponse.user.profile.display_name,
-    avatar: oAuthResponse.user.image_1024,
-    access_token: oAuthResponse.access_token
+    user_id,
+    team_id,
+    name: profile.real_name_normalized,
+    display_name: profile.display_name,
+    avatar: profile.image_1024,
+    access_token: oAuthResponse.authed_user.access_token
   }
 
   const user = await User.findOneAndUpdate({ user_id: userData.user_id }, userData, { new: true, upsert: true, setDefaultsOnInsert: true })
